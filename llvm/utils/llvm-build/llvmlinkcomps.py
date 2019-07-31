4d21e4a732f649370399260b2e09a8cbae91a304
bryant <bryant@users.noreply.github.com>
2019-07-30 17:29:20 -0700

    avoid cmake scoping idiosyncracies.
    Bienemann wanted `LINK_COMPONENTS`.

--- llvm/utils/llvm-build/llvmlinkcomps.py
+++ llvm/utils/llvm-build/llvmlinkcomps.py
@@ -1,8 +1,11 @@
+from __future__ import print_function
 from llvmbuild.main import LLVMProjectInfo
 from os import pardir, path
+import re
 
 root = path.abspath(path.join(path.dirname(__file__), pardir, pardir))
 proj = LLVMProjectInfo.load_from_path(root, root)
+proj.validate_components()  # Populates component_info_map.
 
 for c in proj.component_infos:
     if c.type_name == "TargetGroup" or not hasattr(c, "required_libraries"):
@@ -10,10 +13,11 @@ for c in proj.component_infos:
     cmakelists = path.join(root, c.subpath.lstrip("/"), "CMakeLists.txt")
     cm = open(cmakelists).read()  # Never fails.
 
-    if "LINK_COMPONENTS" in cm:  # Defer to existing specified deps.
-        continue
+    print("Setting link comps for", c.name, c.subpath, repr(cmakelists))
 
-    print("Setting link comps for ", c.name, c.subpath)
+    if "LINK_COMPONENTS" in cm:  # Defer to existing specified deps.
+        print("*LINK_COMPONENTS already present.")
+        continue
 
     req, magic = [], []
     for r in c.required_libraries:
@@ -23,10 +27,30 @@ for c in proj.component_infos:
         elif r.lower() in "native nativecodegen engine".split():
             magic.append(r)
         else:
-            req.append(r)
+            req.append(proj.component_info_map[r].get_library_name())
 
-    join = lambda ss: "".join("\n  " + s for s in sorted(ss))
-    linkcomps = "set(LLVM_LINK_COMPONENTS%s%s\n  )\n" % (join(req), join(magic))
-    open(cmakelists, "w").write(linkcomps + "\n" + cm)
+    pat = r"add_llvm_(?:tool|library|executable|target)\(.+?\)"
+    flags = re.M | re.DOTALL
+    res = re.search(pat, cm, flags=flags)
+    if res is None:
+        print("No add_llvm found in", c.get_library_name(), c.subpath)
 
-    print("Wrote %r" % cmakelists)
+    def repl(m):
+        indent = lambda s: " " * 2 + s
+        try:
+            head, tail = m.group(0).rsplit("\n", 1)
+        except ValueError:
+            head, tail = m.group(0)[:-1], indent(")")
+        assert tail.endswith(")")
+        join = lambda ss: "".join("\n" + indent(s) for s in sorted(ss))
+        linkcomps = "\n\n" + indent("LINK_COMPONENTS")
+
+        if len(req) + len(magic) == 0:
+            linkcomps += "\n\n" + indent("# No component dependencies.\n")
+        else:
+            linkcomps += "\n%s%s\n" % (join(req), join(magic))
+        return head + linkcomps + tail
+    cm = re.sub(pat, repl, cm, count=1, flags=flags)
+    open(cmakelists, "w").write(cm)
+
+    print("Wrote LINK_COMPONENTS.")
